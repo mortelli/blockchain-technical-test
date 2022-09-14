2028a7466fb857cd0205fba2a9a2f4e1231fa492
mortelli <ortellimarcelo@gmail.com>
2022-09-14 12:09:14 -0300

    add CampaignSale contract skeleton

---
+++ contracts/CampaignSale.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./interfaces/ICampaignSale.sol";
+
+/// @title Contract for fundraising campaigns
+/// @author @mortelli
+/// @dev This contract was developed as an technical exercise
+contract CampaignSale is ICampaignSale {
+}
