4c4a239e95d140f6042d164ff6b1c9319f54c8b5
mortelli <ortellimarcelo@gmail.com>
2022-09-15 09:38:06 -0300

    Launch Campaign, Get Campaign (#4)
    * add instructions folder with exercise outline
    
    * init hardhat typescript project
    
    * change solidity version to match exercise
    
    * add ICampaignSale interface
    
    * add solhint config file
    
    * add solhint command
    
    * update package.json with hardhat commands
    
    * add eslint
    
    * add ts lint commands to package.json
    
    * add LICENSE.md file (#2)
    
    * add ts lint commands to package.json
    
    * add LICENSE.md file
    
    * add CampaignSale contract skeleton
    
    * add signatures to CampaignSale contract (no function bodies)
    
    * add openzeppelin contracts dependency
    
    * add counter for  campaign ids, write getCampaign body
    
    * add constructor to CampaignSale contract with erc20 token parameter
    
    * add first iteration for launch campaign method
    
    * add first iteration for deployment test
    
    * add ERC20 dependency contract for tests
    
    * add deployTestErc20 test fixture
    
    * replace hard-coded address in deployment test with test erc20 fixture address
    
    * iterate deployment tests
    
    * reorganize test util files
    
    * add tests for getting maximum campaign length
    
    * extend test utils with deployCampaignSale, daysToSeconds and getCurrentTimeInSeconds functions
    
    * add tokens requirement to launch campaign function in contract
    
    * add first iteration of launch campaign tests
    
    * iterate failure tests for launch campaign
    
    * add comments to launch campaign methods, allow campaign length of exactly maximum campaign length const instead of strictly minor
    
    * add first successful cases for launch campaign tests
    
    * add CampaignParams test interface and launchCampaign helper function, reorder test helper functions, adjust comments in launch campaign test
    
    * add first iteration for get campaign tests
    
    * add ordering to hardhat test and coverage commands through npm

---
+++ contracts/CampaignSale.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./interfaces/ICampaignSale.sol";
+
+/// @title Contract for fundraising campaigns
+/// @author @mortelli
+/// @dev This contract was developed as an technical exercise
+contract CampaignSale is ICampaignSale {
+    using Counters for Counters.Counter;
+
+    /// @notice ERC20 token used to contribute to and fund existing campaigns
+    address public erc20Token;
+
+    /// @dev Counter used for Campaign IDs
+    Counters.Counter private idCounter;
+
+    /// @notice Maximum amount of time a campaign can last
+    uint256 public maximumCampaignLength = 90 days;
+
+    /// @dev Storage for campaigns, running or completed
+    mapping(uint256 => Campaign) private campaigns;
+    
+    /// @param _erc20Token Contract address of the ERC20 token used to contribute to and fund existing campaigns
+    constructor(address _erc20Token){
+        require(_erc20Token != address(0), "erc20 cannot be zero address");
+        erc20Token = _erc20Token;
+    }
+
+    /// @notice Launch a new campaign. 
+    /// @param _goal The goal in token to raise to unlock the tokens for the project
+    /// @param _startAt Starting date of the campaign
+    /// @param _endAt Ending date of the campaign
+    function launchCampaign(
+        uint _goal,
+        uint32 _startAt,
+        uint32 _endAt
+    ) external {
+        require(_goal > 0, "goal must be greater than 0");
+        require(block.timestamp < _startAt, "campaign must start in the future");
+        require(_startAt < _endAt, "campaign must end after it starts");
+        require(_endAt - _startAt <= maximumCampaignLength, "campaign length exceeds maximum"); 
+
+        idCounter.increment();
+        uint256 campaignId = idCounter.current();
+
+        Campaign memory campaign = Campaign({
+            creator: msg.sender,
+            goal: _goal,
+            pledged: 0, // campaigns start with 0 tokens pledged by default
+            startAt: _startAt,
+            endAt: _endAt,
+            claimed: false // campaigns start unclaimed by default
+        });
+        campaigns[campaignId] = campaign;
+
+        emit LaunchCampaign(
+            campaignId,
+            campaign.creator,
+            campaign.goal,
+            campaign.startAt,
+            campaign.endAt
+        );
+    }  
+
+    /// @notice Cancel a campaign
+    /// @param _id Campaign's id
+    function cancelCampaign(uint _id) external {
+
+    }
+
+    /// @notice Contribute to the campaign for the given amount
+    /// @param _id Campaign's id
+    /// @param _amount Amount of the contribution    
+    function contribute(uint _id, uint _amount) external {
+
+    }
+
+    /// @notice Withdraw an amount from your contribution
+    /// @param _id Campaign's id
+    /// @param _amount Amount of the contribution to withdraw
+    function withdraw(uint _id, uint _amount) external {
+
+    }
+
+    /// @notice Claim all the tokens from the campaign
+    /// @param _id Campaign's id
+    function claimCampaign(uint _id) external {
+
+    }
+
+    /// @notice Refund all the tokens to the sender
+    /// @param _id Campaign's id
+    function refundCampaign(uint _id) external {
+
+    }
+
+    /// @notice Get the campaign info
+    /// @param _id Campaign's id
+    function getCampaign(uint _id) external view returns (Campaign memory campaign) {
+        campaign = campaigns[_id];
+        require(campaign.creator != address(0), "campaign does not exist");
+
+        return campaign;
+    }
+}
---
+++ test/utils/consts.ts
@@ -0,0 +1,3 @@
+import { daysToSeconds } from "./funcs";
+
+export const MAXIMUM_CAMPAIGN_LENGTH = daysToSeconds(90);
